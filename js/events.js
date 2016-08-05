0c034885357ec2034eefe27cbe120918ded63ad2
Andriy Gadzevych <hoodlum1983@ukr.net>
2016-08-05 13:31:26 +0300

    update("events.js"): working copy update. Nothing new introduced.

--- js/events.js
+++ js/events.js
@@ -2,6 +2,14 @@ var chk_menu = document.getElementById("chk-menu");
 var as_filter_area = document.getElementById("as-filter-area");
 var div_menubtn = document.getElementById("div-menubtn");
 var div_filtermenu = document.getElementById("div-filtermenu");
+var btn_submit = document.getElementById("btn-submit");
+var frm_record = document.getElementById("frm-record");
+var txt_prjname = document.getElementById("txt-prjname");
+
+
+//document.addEventListener("click", function(event){
+//    console.log(event);
+//});
 
 chk_menu.addEventListener("change", function(event){
     
@@ -9,13 +17,24 @@ chk_menu.addEventListener("change", function(event){
         
         as_filter_area.classList.add("expanded");
         div_menubtn.classList.add("checked");
-        div_filtermenu.style.display = "initial";
+        div_filtermenu.classList.add("visible");
 
     } else{
         
         as_filter_area.classList.remove("expanded");
         div_menubtn.classList.remove("checked");
-        div_filtermenu.style.display = "none";
+        div_filtermenu.classList.remove("visible");
         
     }
 });
+
+//prohibit page refresh on submit
+btn_submit.onclick = function(event){return false;};
+
+btn_submit.addEventListener("click", function(event){
+    console.log("added");
+    return false;
+});
+
+
+txt_prjname.onfocus = function(event){console.log(event);};
